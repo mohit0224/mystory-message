17aa5b65f37548abfc378f200342ee9d35a43dc6
mohit0224 <mohitdheer24+git@gmail.com>
2024-05-07 16:20:10 +0530

    layout.tsx update for home page

--- src/app/(app)/dashboard/layout.tsx
+++ src/app/(app)/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/header/Header";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Mystory Messages",
+	title: "Mystory Messages | Dashboard",
 	description: "Send annonymous messages",
 };
 
